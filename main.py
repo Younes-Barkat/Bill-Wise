55cc9f3842459fb02e4f2f683833eea1cf149240
Younes-Barkat <younesbarkat46@gmail.com>
2026-02-10 17:46:48 +0100

    Added noise_removal

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image
 import cv2
 from matplotlib import pyplot as plt
 
-image_file = "data/doc-078-big.jpg"
+image_file = "data/download.jpg"
 img = cv2.imread(image_file)
 
 # cv2.imshow("Original image", img)
@@ -23,12 +23,12 @@ def display(im_path): #from stackoverflow
      ax.imshow(im_data, cmap='gray')
      plt.show()
 
-display(image_file)
+# display(image_file)
 
 inverted_image = cv2.bitwise_not(img)
 cv2.imwrite("temp/inverted.jpg", inverted_image)
 
-display("temp/inverted.jpg")
+# display("temp/inverted.jpg")
 
 def grayscale(image):
 
@@ -37,6 +37,28 @@ def grayscale(image):
 gray_image = grayscale(img)
 cv2.imwrite("temp/gray.jpg", gray_image)
 
-display("temp/gray.jpg")
+# display("temp/gray.jpg")
+
+thresh, im_bw = cv2.threshold(gray_image, 150, 170, cv2.THRESH_BINARY)
+cv2.imwrite("temp/im_bw.jpg", im_bw)
+# display("temp/im_bw.jpg")
+
+def noise_removal(image):
+     import numpy as np
+     kernel = np.ones((1, 1), np.uint8)
+     image = cv2.dilate(image, kernel, iterations=1)
+     kernel = np.ones((1, 1), np.uint8)
+     image = cv2.erode(image, kernel, iterations=1)
+     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+     image = cv2.medianBlur(image, 3)
+     return image
+
+no_noise = noise_removal(im_bw)
+cv2.imwrite("temp/no_noise.jpg", no_noise)
+display("temp/no_noise.jpg")
+
+
+
+
 
 
